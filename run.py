9797b888c9c546d47ea5330b6130632f55b9f8c8
liuyun16 <larry1695@163.com>
2017-11-01 01:30:20 +0100

    local run OK

--- run.py
+++ run.py
@@ -1,15 +1,7 @@
-#启动web服务器脚本
 from app import app
 import os
 
 if __name__ == '__main__':
-<<<<<<< HEAD
+    # 服务器用
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-=======
-    #服务器用
-    # port = int(os.environ.get('PORT', 5000))
-    # app.run(host='0.0.0.0', port=port)
-    #本地调试用
-    app.run(debug = True)
->>>>>>> b97e3b9ca4dff88eb4a6866eddc929ea33193b36
